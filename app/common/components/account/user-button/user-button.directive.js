c1de3cd50f25e824896326421ea5741637e8ac53
Xavier Bustamante Talavera <xavier@bustawin.com>
2016-11-16 17:52:25 +0100

    Gravatar use timmy as default image and fix border color

--- app/common/components/account/user-button/user-button.directive.js
+++ app/common/components/account/user-button/user-button.directive.js
@@ -1,9 +1,10 @@
-function userButton (session, $uibModal) {
+function userButton (session, $uibModal, CONSTANTS) {
   return {
     templateUrl: window.COMPONENTS + '/account/user-button/user-button.directive.html',
     restrict: 'E',
     scope: {},
     link: function ($scope, $element, $attrs) {
+      $scope.DEFAULT_AVATAR = encodeURI('https://data.ereuse.org/images/timmy/avatar.png')
       $scope.account = session.getAccount()
       $scope.openModal = function (type) {
         var modalInstance = $uibModal.open({
