631606a0b70cb2bfbbdc5bb34296fe7bb1bf1a20
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-03-31 10:26:24 +0200

    Add clickout to closePopover to close them when popover's trigger is 'none'

--- app/common/components/close-popover.js
+++ app/common/components/close-popover.js
@@ -1,13 +1,30 @@
+require('jquery-clickout')
+
 function closePopover () {
+  /**
+   * @ngdoc directive
+   * @description Adds a 'x' to close the popover, and a way to automatically close it when user clicks outside it.
+   * @param {object} popover
+   * @param {bool} popover.isOpen - A flag to change the state of the popover
+   * @param {bool} [closeOnClickout=false] - A flag setting if the popover should be closed when clicking out of it.
+   * Note
+   * that this only makes sense when we set the 'trigger' of the popover to 'none', otherwise use the built-in trigger
+   * 'clickout'.
+   */
   return {
     template: '<i class="fa fa-lg fa-border fa-times" ng-click="closePopover()"></i>',
     restrict: 'E',
     scope: {
-      popover: '='
+      popover: '=',
+      closeOnClickout: '=?'
     },
-    link: function ($scope) {
+    link: function ($scope, $element) {
       $scope.closePopover = function () {
         $scope.popover.isOpen = false
+      }
+      // When the user clicks out of the popup, we close it
+      if ($scope.closeOnClickout) {
+        setTimeout(() => { $element.closest('.popover-inner').on('clickout', $scope.closePopover) }, 50)
       }
     }
   }
