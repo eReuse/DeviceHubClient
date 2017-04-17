feab5f9e38299ff3bd1f1589b25432152cabb176
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-04-17 20:06:52 +0200

    Reformat ResourceSettings to ES6

--- app/common/components/resource/resource-settings.factory.js
+++ app/common/components/resource/resource-settings.factory.js
@@ -1,13 +1,3 @@
-var utils = require('./../utils')
-
-var Unauthorized = require('./../authentication/Unauthorized')
-
-/**
- * @typedef {Object} ResourceSettingsRet
- * @property {Function} of The ResourceSettings method
- * @property {$q.defer} loaded A promise indicating if all resources are ready to use
- */
-
 /**
  * A factory for resourceSettings. Once you import ResourceSettings, just do:
  *
@@ -20,9 +10,10 @@ var Unauthorized = require('./../authentication/Unauthorized')
  *
  * To know when ResourceSettings in are ready to use, do ResourceSettings.loaded.then(f())
  *
- * @return {ResourceSettingsRet}
+ * @return {_ResourceSettingsFactory}
  */
 function resourceSettingsFactory (ResourceServer, schema, RESOURCE_CONFIG) {
+  const utils = require('./../utils')
   /**
    * Similar to DeviceHub's ResourceSettings, it stores several information about a resource. It can do:
    * - Stores settings and schema
@@ -47,88 +38,83 @@ function resourceSettingsFactory (ResourceServer, schema, RESOURCE_CONFIG) {
    * not an object of
    * devices:deviceEvent).
    */
-  function ResourceSettings (type) {
-    if (!_.isString(type)) throw TypeError('ResourceSettings: type is expected to be string, but it is ' + typeof type)
-    if (utils.Naming.type(type) !== type) throw TypeError(type + ' should be of Type convention.')
-    this.type = type
-    this.resourceName = utils.Naming.resource(type)
-    this.humanName = utils.Naming.humanize(type)
-    this.settings = {}
-    this.authorized = this.resourceName in schema.schema // The server only sends the schemas that we have access to
-    if (this.authorized) {
-      this.schema = schema.schema[this.resourceName]
-      this.settings = _.assign(this._getInnerSettings(), this.schema._settings)
-      this.server = ResourceServer(this.settings)
-      this.subResourcesNames = _.without(this.schema['@type']['allowed'], this.type)
-      this.isALeaf = this.subResourcesNames.length === 0 || this.type === 'devices:EraseBasic' // todo so works redo
+  class ResourceSettings {
+    /**
+     * @param {string} type - The resource type in Type convention.
+     * @throw {TypeError} When type is not string.
+     * @throw {TypeError} When type is not in Type convention.
+     */
+    constructor (type) {
+      if (!_.isString(type)) throw TypeError('ResourceSettings: type is expected to be string, but it is ' + typeof type)
+      if (utils.Naming.type(type) !== type) throw TypeError(type + ' should be of Type convention.')
+      this.type = type
+      this.resourceName = utils.Naming.resource(type)
+      this.humanName = utils.Naming.humanize(type)
+      this.settings = {}
+      this.authorized = this.resourceName in schema.schema // The server only sends the schemas that we have access to
+      if (this.authorized) {
+        this.schema = schema.schema[this.resourceName]
+        this.settings = _.assign(this._getInnerSettings(), this.schema._settings)
+        this.server = ResourceServer(this.settings)
+        this.subResourcesNames = _.without(this.schema['@type']['allowed'], this.type)
+        this.isALeaf = this.subResourcesNames.length === 0 || this.type === 'devices:EraseBasic' // todo so works redo
+      }
     }
-  }
 
-  var rs = ResourceSettings.prototype
+    _getInnerSettings () {
+      return RESOURCE_CONFIG.resources[this.type] || {}
+    }
 
-  rs._getInnerSettings = function () {
-    return RESOURCE_CONFIG.resources[this.type] || {}
-  }
+    /**
+     * Gets full resourceSettings for all subresources (see property subResourcesName).
+     * @return {Array}
+     */
+    getSubResources () {
+      return _.map(this.subResourcesNames, subResourceName => _ResourceSettingsFactory(subResourceName))
+    }
 
-  /**
-   * Gets full resourceSettings for all subresources (see property subResourcesName).
-   * @return {Array}
-   */
-  rs.getSubResources = function () {
-    var subResources = []
-    _.forEach(this.subResourcesNames, function (subResourceName) {
-      subResources.push(_ResourceSettingsFactory(subResourceName))
-    })
-    return subResources
-  }
+    /**
+     * Returns if the actual type is a sub-resource of a given parent type.
+     *
+     * The parent type is not a subResource of itthis.
+     *
+     * @param {string} parentType In ResourceType format.
+     * @return {boolean}
+     */
+    isSubResource (parentType) {
+      return _.includes(_ResourceSettingsFactory(parentType).subResourcesNames, this.type)
+    }
 
-  /**
-   * Returns if the actual type is a sub-resource of a given parent type.
-   *
-   * The parent type is not a subResource of itthis.
-   *
-   * @param {string} parentType In ResourceType format.
-   * @return {boolean}
-   */
-  rs.isSubResource = function (parentType) {
-    return _.includes(_ResourceSettingsFactory(parentType).subResourcesNames, this.type)
-  }
+    /**
+     * As *isSubResource* but including itself.
+     * @param {string} parentType
+     * @returns {boolean}
+     */
+    isSubResourceOrItself (parentType) {
+      return parentType === this.type || this.isSubResource(parentType)
+    }
 
-  /**
-   * As *isSubResource* but including itthis.
-   * @param {string} parentType
-   * @returns {boolean}
-   */
-  rs.isSubResourceOrItthis = function (parentType) {
-    return parentType === this.type || this.isSubResource(parentType)
-  }
-
-  rs.throwError = function () {
-    throw Unauthorized('The user is not authorized to submit this resource in DeviceHub.')
-  }
-
-  /**
-   * Gets the setting in the settings of the passed-in resource or, if the resource has not
-   * the setting, it gets it from one of its ancestors.
-   *
-   * The value of the setting cannot be 'undefined'.
-   *
-   * @param {string} path - The name of the setting to fetch.
-   * @throw {TypeError} Nor the resource type of its ancestors had the setting.
-   * @return {*} The value of the setting
-   */
-  rs.getSetting = function (path) {
-    return utils.getSetting(RESOURCE_CONFIG.resources, this, path)
+    /**
+     * Gets the setting in the settings of the passed-in resource or, if the resource has not
+     * the setting, it gets it from one of its ancestors.
+     *
+     * The value of the setting cannot be 'undefined'.
+     *
+     * @param {string} path - The name of the setting to fetch.
+     * @throw {TypeError} Nor the resource type of its ancestors had the setting.
+     * @return {*} The value of the setting
+     */
+    getSetting (path) {
+      return utils.getSetting(RESOURCE_CONFIG.resources, this, path)
+    }
   }
 
   // ResourceSettings are singletons per resource, so we avoid duplicities for the same resource
   // This is the actual factory
-  var resourceTypes = {}
+  let resourceTypes = {}
 
   function _ResourceSettingsFactory (type) {
-    if (!(type in resourceTypes)) {
-      resourceTypes[type] = new ResourceSettings(type)
-    }
+    if (!(type in resourceTypes)) resourceTypes[type] = new ResourceSettings(type)
     return resourceTypes[type]
   }
 
