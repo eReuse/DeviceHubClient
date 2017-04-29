e3345918f0418ed3d08c5ee1cbe5ff9e0edfc7b6
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-04-29 19:23:30 +0200

    Remove spinner from map; remove device-event

--- app/common/components/device/index.js
+++ app/common/components/device/index.js
@@ -16,7 +16,6 @@ module.exports = angular.module('common.components.device',
     require('./../authentication').name,
     require('./certificate').name,
     require('./../forms').name,
-    require('./events-per-device').name,
     require('./device-label').name
   ])
   /**
