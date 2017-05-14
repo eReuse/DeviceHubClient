d38c5815cd5912b320ec55b9b3629859062d7641
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-05-14 18:04:49 +0200

    Export groups

--- app/common/components/resource/resource-list/resource-list-footer/resource-list-footer.directive.js
+++ app/common/components/resource/resource-list/resource-list-footer/resource-list-footer.directive.js
@@ -4,12 +4,13 @@
  * @param {object|undefined} resource
  * undefined evaluates to true.
  */
-function resourceListFooter (session, CONSTANTS, $http, ResourceListGetterBig, ResourceListSelectorBig) {
+function resourceListFooter (session, CONSTANTS, $http, ResourceListGetterBig, ResourceListSelectorBig,
+                             ResourceSettings) {
   return {
     templateUrl: require('./__init__').PATH + '/resource-list-footer.directive.html',
     restrict: 'E',
     scope: {},
-    link: function ($scope) {
+    link: $scope => {
       $scope.UIB_TOOLTIP_TEMPLATE = require('./__init__').PATH + '/selected-resources.uib-tooltip-template.html'
 
       ResourceListGetterBig.callbackOnGetting((__, ___, pag) => { $scope.totalResources = pag.totalPages })
@@ -19,24 +20,24 @@ function resourceListFooter (session, CONSTANTS, $http, ResourceListGetterBig, R
         $scope.numberSelectedResourcesInList = inList.length
       })
 
-      let MIME_TYPES = {
+      const MIME_TYPES = {
         xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         ods: 'application/vnd.oasis.opendocument.spreadsheet'
       }
       $scope.isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/)
-      $scope.exportSpreadsheet = function (format) {
-        let Naming = require('./../../../utils').Naming
-        let saveAs = require('file-saver').saveAs
-        let mimeType = MIME_TYPES[format]
-        let resource = Naming.resource('Device')
+      $scope.exportSpreadsheet = format => {
+        const saveAs = require('file-saver').saveAs
+        const mimeType = MIME_TYPES[format]
+        const rSettings = ResourceSettings($scope.selectedResources[0]['@type'])
+        const resource = rSettings.resourceName
         $http({
           method: 'GET',
           url: CONSTANTS.url + '/' + session.activeDatabase + '/export/' + resource,
-          params: {'ids': _.map($scope.selectedResources, '_id'), 'groupBy': 'Actual place'},
+          params: {'ids': _.map($scope.selectedResources, rSettings.isSubResource('Group') ? 'label' : '_id')},
           headers: {'Accept': mimeType, 'Authorization': 'Basic ' + session.getAccount().token},
           responseType: 'arraybuffer'
-        }).success((data) => {
-          let file = new File([data], resource + '.' + format, {type: mimeType})
+        }).success(data => {
+          const file = new File([data], resource + '.' + format, {type: mimeType})
           saveAs(file)
         })
       }
