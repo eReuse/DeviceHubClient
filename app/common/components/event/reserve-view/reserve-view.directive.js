9f46522437c216cf7c60070ec3ef7283fc30b46a
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-10-17 12:42:32 +0200

    Not try to fulfill 'to' field in Sell as it is incompatible with get-from-data-relation-or-create

--- app/common/components/event/reserve-view/reserve-view.directive.js
+++ app/common/components/event/reserve-view/reserve-view.directive.js
@@ -8,29 +8,16 @@ function reserveView (ResourceSettings, dhModal, ReserveFormSchema, session) {
     link: $scope => {
       $scope.session = session
       const openEventModal = require('./../open-event-modal')(ResourceSettings, dhModal)
-      $scope.account = {'@type': 'Account', _id: $scope.resource.for || $scope.resource.byUser}
+      $scope.account = {'@type': 'Account', _id: $scope.resource.for}
       const deviceSettings = ResourceSettings('Device')
       const options = {FormSchema: ReserveFormSchema}
       // $scope.resource might not be totally defined when accessing this view through the URL
       const query = () => ({where: {_id: {$in: $scope.resource.devices}}})
       let promiseDevices
-      $scope.sell = () => {
+      $scope.openModal = type => {
         if (!promiseDevices) promiseDevices = deviceSettings.server.getList(query())
         promiseDevices.then(devices => {
-          const model = {
-            reserve: $scope.resource._id,
-            to: $scope.resource.for
-          }
-          openEventModal('devices:Sell', devices, model, options)
-        })
-      }
-      $scope.cancelReservation = () => {
-        if (!promiseDevices) promiseDevices = deviceSettings.server.getList(query())
-        promiseDevices.then(devices => {
-          const model = {
-            reserve: $scope.resource._id
-          }
-          openEventModal('devices:CancelReservation', devices, model, options)
+          openEventModal(type, devices, {reserve: $scope.resource._id}, options)
         })
       }
       if ($scope.resource.sell || $scope.resource.cancel) {
