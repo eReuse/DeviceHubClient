8ae77a7fb560e1313f8c749a08bd8947de4c0b48
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-10-23 15:28:57 +0200

    Allow shared users to cancel reservations; fix adding device to cancelReserve and sell events

--- app/common/components/event/open-event-modal.js
+++ app/common/components/event/open-event-modal.js
@@ -11,12 +11,13 @@ function openModalFactory (ResourceSettings, dhModal) {
   /**
    * Opens the modal.
    * @param {string} type - The @type of the event to perform.
-   * @param {Object[]} resources - devices xor groups to perform the event on.
-   * @param {Object} model - Optional. Pre-built model where type and resources are appended. This param is mutated.
+   * @param {Object[]|null} resources - devices or groups to perform the event on.
+   * @param {Object} model - Optional. Pre-built model where type and resources are appended. This param is
+   * mutated.
    * @param {Object} options - Opotional. Options to pass to FormSchema.
    */
-  return (type, resources, model = {}, options = {}) => {
-    const isGroup = ResourceSettings(resources[0]['@type']).isSubResource('Group')
+  return (type, resources = null, model = {}, options = {}) => {
+    const isGroup = resources && ResourceSettings(resources[0]['@type']).isSubResource('Group')
     // We make it: groups.lots = [objLot1, objLot2...]
     if (isGroup) {
       resources = _(resources).groupBy('@type').mapKeys((_, type) => Naming.resource(type)).value()
@@ -24,11 +25,11 @@ function openModalFactory (ResourceSettings, dhModal) {
       _.arrayExtend(resources['lots'], _.pop(resources, 'incoming-lot', []))
       _.arrayExtend(resources['lots'], _.pop(resources, 'outgoing-lot', []))
     }
+    if (resources) model[isGroup ? 'groups' : 'devices'] = resources
     model['@type'] = type
-    model[isGroup ? 'groups' : 'devices'] = resources
     const opt = {
       model: () => model,
-      parserOptions: () => ({doNotUse: isGroup ? ['devices'] : ['groups']}),
+      parserOptions: () => ({doNotUse: resources ? (isGroup ? ['devices'] : ['groups']) : ['devices', 'groups']}),
       options: () => options
     }
     dhModal.open('form', opt)
--- app/common/components/event/reserve-view/reserve-view.directive.js
+++ app/common/components/event/reserve-view/reserve-view.directive.js
@@ -9,16 +9,10 @@ function reserveView (ResourceSettings, dhModal, ReserveFormSchema, session) {
       $scope.session = session
       const openEventModal = require('./../open-event-modal')(ResourceSettings, dhModal)
       $scope.account = {'@type': 'Account', _id: $scope.resource.for}
-      const deviceSettings = ResourceSettings('Device')
       const options = {FormSchema: ReserveFormSchema}
       // $scope.resource might not be totally defined when accessing this view through the URL
-      const query = () => ({where: {_id: {$in: $scope.resource.devices}}})
-      let promiseDevices
       $scope.openModal = type => {
-        if (!promiseDevices) promiseDevices = deviceSettings.server.getList(query())
-        promiseDevices.then(devices => {
-          openEventModal(type, devices, {reserve: $scope.resource._id}, options)
-        })
+        openEventModal(type, null, {reserve: $scope.resource._id}, options)
       }
       if ($scope.resource.sell || $scope.resource.cancel) {
         $scope.finalState = $scope.resource.sell ? 'devices:Sell' : 'devices:Cancel'
