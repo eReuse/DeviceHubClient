ec7a8197ea836212122c06da57c50c21c2d33062
Stephan Fortelny <stephanfortelny@gmail.com>
2018-07-05 12:24:47 +0200

    WIP: Moved events for manual events button to button directive

--- app/common/components/event/manual-events-button/manual-events-button.directive.js
+++ app/common/components/event/manual-events-button/manual-events-button.directive.js
@@ -5,7 +5,20 @@ function manualEventsButton (ResourceSettings, dhModal, ResourceListSelector) {
     scope: {},
     link: $scope => {
       $scope.selector = ResourceListSelector
-      $scope.events = ResourceSettings('devices:DeviceEvent').getSubResources()
+      $scope.events = [
+        {'type': 'devices:Ready', 'humanName': 'Ready'},
+        {'type': 'devices:ToPrepare', 'humanName': 'To prepare'},
+        {'type': 'devices:Dispose', 'humanName': 'Dispose'},
+        {'type': 'devices:Locate', 'humanName': 'Locate'},
+        {'type': 'devices:ToDispose', 'humanName': 'To dispose'},
+        {'type': 'devices:Sell', 'humanName': 'Sell'},
+        {'type': 'devices:Reserve', 'humanName': 'Reserve'},
+        {'type': 'devices:Repair', 'humanName': 'Repair'},
+        {'type': 'devices:Deallocate', 'humanName': 'Deallocate'},
+        {'type': 'devices:Receive', 'humanName': 'Receive'},
+        {'type': 'devices:Allocate', 'humanName': 'Allocate'},
+        {'type': 'devices:ToRepair', 'humanName': 'To repair'}
+      ]
       // If the passed-in resources are groups, we won't use the 'devices' field of the event, and otherwise
 
       $scope.openModal = (eventType) => {
