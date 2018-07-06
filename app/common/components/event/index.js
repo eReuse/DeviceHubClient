3116336ebb2a07b6fa9015f2e7e4ab6423a8ecdf
Stephan Fortelny <stephanfortelny@gmail.com>
2018-07-06 10:14:18 +0200

    WIP Cleanup: Remove unused reserve-button directive + template

--- app/common/components/event/index.js
+++ app/common/components/event/index.js
@@ -10,7 +10,6 @@ module.exports = angular.module('common.components.event',
   .directive('manualEventsButton', require('./manual-events-button/manual-events-button.directive.js'))
   .directive('eventEasyExplanation', require('./event-easy-explanation/event-easy-explanation.directive.js'))
   .directive('eventExplanation', require('./event-explanation/event-explanation.directive.js'))
-  .directive('reserveButton', require('./reserve-button/reserve-button.directive'))
   .directive('reserveView', require('./reserve-view/reserve-view.directive'))
   .directive('sellView', require('./sell-view/sell-view.directive'))
   .factory('ReserveFormSchema', require('./reserve.form-schema.factory'))
