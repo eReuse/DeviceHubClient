cfb6c9df29b382b7e6cf8a0ab3e2f4a9cdcc7c9e
Stephan Fortelny <stephanfortelny@gmail.com>
2018-07-06 10:26:30 +0200

    WIP Remove ResourceSettings: Simplified resource-icon by excluding device-icon (use device-icon directive directly) and removing arrow right

--- app/common/components/resource/resource-icon/resource-icon.directive.js
+++ app/common/components/resource/resource-icon/resource-icon.directive.js
@@ -3,15 +3,11 @@ function resourceIcon (ResourceSettings) {
     templateUrl: require('./__init__').PATH + '/resource-icon.directive.html',
     restrict: 'E',
     scope: {
-      resourceType: '@',
-      resourceSubtype: '@?',
-      fillTo: '=' // If defined == true
+      resourceType: '@'
     },
     link: function ($scope) {
       let rSettings = ResourceSettings($scope.resourceType)
       $scope.settings = rSettings.settings
-      $scope.isDevice = $scope.resourceType === 'Device' || rSettings.isSubResource('Device')
-      $scope.to = _.includes($scope.resourceType, 'To')
     }
   }
 }
