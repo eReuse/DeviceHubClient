15f6d7835e98f73a795e5b3d4a8f965a70ad2118
Stephan Fortelny <stephanfortelny@gmail.com>
2018-09-18 14:52:38 +0200

    Disabled transfer asset license button for now

--- app/common/components/event/manual-events-button/manual-events-button.directive.js
+++ app/common/components/event/manual-events-button/manual-events-button.directive.js
@@ -24,7 +24,7 @@ function manualEventsButton (dhModal, ResourceListSelector) {
         {'type': 'devices:Receive', 'humanName': 'Receive'},
         {'type': 'devices:Allocate', 'humanName': 'Allocate'},
         {'type': 'devices:ToRepair', 'humanName': 'To repair'},
-        {'type': 'devices:TransferAssetLicense', 'humanName': 'Transfer asset license'},
+        // {'type': 'devices:TransferAssetLicense', 'humanName': 'Transfer asset license'},
         {'type': 'devices:Snapshot', 'humanName': 'Add tag', 'singleSelectionOnly': true}
       ]
 
