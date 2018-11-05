d825e5e6d42d152134fea603bab95e0c7051dd9e
Xavier Bustamante Talavera <xavier@bustawin.com>
2018-11-05 12:13:35 +0100

    Remove certificate

--- app/common/components/device/certificate/certificate.modal.controller.js
+++
@@ -1,8 +0,0 @@
-function certificateModalCtrl ($scope, $uibModalInstance, resources, title) {
-  $scope.resources = resources
-  $scope.title = title
-  $scope.status = {}
-  $scope.cancel = () => $uibModalInstance.dismiss('cancel')
-}
-
-module.exports = certificateModalCtrl
