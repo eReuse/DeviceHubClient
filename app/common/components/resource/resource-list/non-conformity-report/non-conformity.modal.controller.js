40e30de7107429deacd17673871f4fe9ce67f536
Stephan Fortelny <stephanfortelny@gmail.com>
2018-11-09 12:52:55 +0100

    WIP Added input label

--- app/common/components/resource/resource-list/non-conformity-report/non-conformity.modal.controller.js
+++ app/common/components/resource/resource-list/non-conformity-report/non-conformity.modal.controller.js
@@ -1,11 +1,11 @@
-function certificateModalCtrl ($scope, $uibModalInstance, devices) {
+function certificateModalCtrl ($scope, $uibModalInstance, Notification, devices) {
   $scope.ids = ''
   $scope.calculate = () => {
     const expectedIds = $scope.ids.split(',').map(s => s.trim())
     const actualIds = devices.map(r => r.serialNumber)
     $scope.missing = _.difference(expectedIds, actualIds)
     $scope.additional = _.difference(actualIds, expectedIds)
-    Notification.success('Non-conformities')
+    Notification.success('Non-conformities calculated')
   }
   $scope.cancel = () => $uibModalInstance.dismiss('cancel')
 }
