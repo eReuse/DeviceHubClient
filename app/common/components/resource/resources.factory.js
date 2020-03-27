27b8f932660c88b4cdb425633522cafd46bedb4b
Stephan Fortelny <s.fortelny@ereuse.org>
2020-03-27 17:37:23 +0100

    Set ProofFunction.diskUsage to value of TestStorage.lifetime - fixes issue https://github.com/eReuse/DeviceHubClient/issues/29

--- app/common/components/resource/resources.factory.js
+++ app/common/components/resource/resources.factory.js
@@ -1759,14 +1759,16 @@ function resourceFactory ($rootScope, server, CONSTANTS, $filter, enums, URL) {
 
     static createFromDevice(device, author) {
       const rate = device.rate
+      const testDataStorage = device.actions.find(a => a.lifetime)
+      const lifetime = testDataStorage ? testDataStorage.lifetime : 0
       if(rate) {
         const data = _.assign(Proof.createFromDevice(device), {
-          diskUsage: 0,  // TODO server must return this prop
+          diskUsage: lifetime, 
           proofAuthor: author.ethereum_address,
           proofAuthorID: author.id,
           rateID: rate.id,
           score: rate.rating.value,
-          algorithmVersion: 'v1' // TODO server must return this prop
+          algorithmVersion: 'v1'
         })
 
         return new ProofFunction(data)
