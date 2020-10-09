194273820afbccf033fcb8ef3d12e269f3e6c98f
Stephan Fortelny <s.fortelny@ereuse.org>
2020-10-09 16:44:16 +0200

    Fixed unpacking response from GET /<schema>/versions to set server and tags versions

--- app/common/components/account/user-button/user-button.directive.js
+++ app/common/components/account/user-button/user-button.directive.js
@@ -7,10 +7,15 @@ function userButton (session, server, $rootScope) {
     link: $scope => {
       $scope.user = session.user
       $scope.logout = () => session.logout()
-      new server.DevicehubThing('/versions/').get('').then(result => {
+      const versionsResource = { //TODO convert to proper ressource
+        init: (data) => {
+          return data
+        }
+      }
+      new server.DevicehubThing('/versions/', versionsResource).get('').then(result => {
         $scope.appVersions = $rootScope.appVersions
-        $rootScope.appVersions.server = result.data.devicehub
-        $rootScope.appVersions.tags = result.data.ereuse_tag
+        $rootScope.appVersions.server = result.devicehub
+        $rootScope.appVersions.tags = result.ereuse_tag
       })  
     }
   }
