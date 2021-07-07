ac99dd29bb8a0c843145fd4c11e2fe6dcbc959c2
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-07-07 11:08:56 +0200

    open security

--- app/views/inventory/upload.snapshot.controller.js
+++ app/views/inventory/upload.snapshot.controller.js
@@ -47,7 +47,9 @@ function uploadSnapshotCtl ($scope, fields, resources, $translate, server, $stat
       }
       // Upload Snapshot
       const snapshot = resources.Snapshot.init(raw, false)
-      snapshot.data = raw.data
+      if (!snapshot.version) {
+        snapshot.data = raw.data
+      }
       return snapshot.post().then(() => {
         this.results.push(new Success(file, snapshot))
       }).catch(e => {
