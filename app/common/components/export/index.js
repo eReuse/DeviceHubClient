692d5945b0d53071f700a8908c503927c3cc9b28
Cayo Puigdefabregas <cayo@ribaguifi.com>
2021-12-07 13:21:32 +0100

    adding buttom download metrics in trade lots

--- app/common/components/export/index.js
+++ app/common/components/export/index.js
@@ -1,4 +1,5 @@
 module.exports = angular.module('common.components.export',
   [])
   .directive('exportButton', require('./export-button/export-button.directive'))
+  .directive('exportMetricsLot', require('./export-metrics-lot/export-metrics-lot.directive'))
   .directive('importButton', require('./import-button/import-button.directive'))
